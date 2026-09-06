2645be513d0e2a500d1857dd7b486538efba1448
Arman Ahmed <arman.ahmed.code@gmail.com>
2026-09-06 23:11:01 +0600

    feat: add navbar sidebar and footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+    return(
+        <>
+        
+        <div className="">This is footer</div>
+        </>
+    )
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar () {
+    return(
+        <>
+        
+        <div className="">This is navbar</div>
+        </>
+    )
+}
